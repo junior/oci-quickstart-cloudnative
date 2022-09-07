5529fff79e248456bdd21d059e98f6446dedc6fa
junior <junior@users.noreply.github.com>
2022-09-07 02:18:18 -0500

    moving oke to a module under oke-base repo
    Signed-off-by: junior <junior@users.noreply.github.com>

--- deploy/complete/terraform/providers.tf
+++ deploy/complete/terraform/providers.tf
@@ -1,39 +1,39 @@
-# Copyright (c) 2020-2022 Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
 # 
 
 terraform {
-  required_version = ">= 1.0"
+  required_version = ">= 1.1"
   required_providers {
     oci = {
       source  = "oracle/oci"
-      version = ">= 4.85.0"
-      # https://registry.terraform.io/providers/hashicorp/oci/4.85.0
+      version = "~> 4, < 5"
+      # https://registry.terraform.io/providers/oracle/oci/
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.2.0" # Latest version as January 2022 = 2.7.1. Using 2.2.0 (May, 2021) for ORM compatibility (12 releases behind)
-      # https://registry.terraform.io/providers/hashicorp/kubernetes/2.2.0
+      version = "~> 2"
+      # https://registry.terraform.io/providers/hashicorp/kubernetes/
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "2.1.0" # Latest version as January 2022 = 2.4.1. Using 2.1.0 (March, 2021) for ORM compatibility (7 releases behind)
-      # https://registry.terraform.io/providers/hashicorp/helm/2.1.0
+      version = "~> 2"
+      # https://registry.terraform.io/providers/hashicorp/helm/
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "3.1.0" # Latest version as January 2022 = 3.1.0.
-      # https://registry.terraform.io/providers/hashicorp/tls/3.1.0
+      version = "~> 4"
+      # https://registry.terraform.io/providers/hashicorp/tls/
     }
     local = {
       source  = "hashicorp/local"
-      version = "2.1.0" # Latest version as January 2022 = 2.1.0.
-      # https://registry.terraform.io/providers/hashicorp/local/2.1.0
+      version = "~> 2"
+      # https://registry.terraform.io/providers/hashicorp/local/
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.1.0" # Latest version as January 2022 = 3.1.0.
-      # https://registry.terraform.io/providers/hashicorp/random/3.1.0
+      version = "~> 3"
+      # https://registry.terraform.io/providers/hashicorp/random/
     }
   }
 }
@@ -91,8 +91,10 @@ provider "helm" {
 }
 
 locals {
-  cluster_endpoint       = yamldecode(data.oci_containerengine_cluster_kube_config.oke.content)["clusters"][0]["cluster"]["server"]
-  cluster_ca_certificate = base64decode(yamldecode(data.oci_containerengine_cluster_kube_config.oke.content)["clusters"][0]["cluster"]["certificate-authority-data"])
-  cluster_id             = yamldecode(data.oci_containerengine_cluster_kube_config.oke.content)["users"][0]["user"]["exec"]["args"][4]
-  cluster_region         = yamldecode(data.oci_containerengine_cluster_kube_config.oke.content)["users"][0]["user"]["exec"]["args"][6]
+  cluster_endpoint = (var.cluster_endpoint_visibility == "Private") ? (
+    "https://${module.oke.orm_private_endpoint_oke_api_ip_address}:6443") : (
+  yamldecode(module.oke.kubeconfig)["clusters"][0]["cluster"]["server"])
+  cluster_ca_certificate = base64decode(yamldecode(module.oke.kubeconfig)["clusters"][0]["cluster"]["certificate-authority-data"])
+  cluster_id             = yamldecode(module.oke.kubeconfig)["users"][0]["user"]["exec"]["args"][4]
+  cluster_region         = yamldecode(module.oke.kubeconfig)["users"][0]["user"]["exec"]["args"][6]
 }
